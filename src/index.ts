96429737c2461ac8e0107d6f967b97fd11b051e2
ryuncoders <seungjy10@gmail.com>
2024-06-27 11:21:02 +0900

    feat(Blockchain): Blockchain class 생성 / []의 활용

--- src/index.ts
+++ src/index.ts
@@ -21,3 +21,37 @@ class Block implements BlockShape {
     return crypto.createHash("sha256").update(toHash).digest("hex");
   }
 }
+
+class Blockchain {
+  private blocks: Block[];
+  constructor() {
+    this.blocks = [];
+  }
+  getPrevHash() {
+    if (this.blocks.length === 0) return "";
+    return this.blocks[this.blocks.length - 1].hash;
+  }
+  addBlock(data: string) {
+    const newBlock = new Block(
+      this.getPrevHash(),
+      this.blocks.length + 1,
+      data
+    );
+    this.blocks.push(newBlock);
+  }
+  getBlocks() {
+    return [...this.blocks];
+  }
+}
+
+const blockchain = new Blockchain();
+blockchain.addBlock("First one");
+blockchain.addBlock("Second one");
+blockchain.addBlock("Third one");
+blockchain.addBlock("Fourth one");
+
+// blockchain.getBlocks().push(new Block("xxxxx", 12345, "fack"));
+// this.blocks -> [...this.blocks]로 위의 코드 방어
+// []생성이 되면 바로 return 하는 것과 마찬가지기 때문에 .push가 실행되지 않음.
+
+console.log("???", blockchain.getBlocks());
